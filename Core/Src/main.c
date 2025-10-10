dd200b27f1d99f7305b8d8f5cd1ce58c54737768
Milica Makevic <mmakevic1999@gmail.com>
2025-10-11 00:04:12 +0200

    Setup printf to redirect output to UART

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -23,7 +23,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "stdio.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -97,7 +97,9 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+	  printf("Hello!");
+	  printf("\n");
+	  HAL_Delay(1000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
